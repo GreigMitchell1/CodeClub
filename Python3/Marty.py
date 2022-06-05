b5c424e4ee630d4257063897252a646ee47f3992
Greig MitchelL <greigmitchell@me.com>
2022-06-05 14:14:44 +0100

    Added Functionality to Marty

--- Python3/Marty.py
+++ Python3/Marty.py
@@ -2,6 +2,7 @@ from martypy import Marty
 import os 
 import time
 import subprocess
+import keyboard
 
 MartyIP = "192.168.4.1" #example
 
@@ -20,6 +21,7 @@ def martyalive():
   else:
    print (MartyIP, 'is down!')
   return response
+  marty.get_battery_voltage()
 
 #Clear marty calibration details, and save new - DO NOT USE YET!
 def martycalibrate():
@@ -29,37 +31,61 @@ def martycalibrate():
 
 #Make marty walk by # of steps, -ive values possible
 def martywalk(steps):
-  marty.walk(num_steps=steps)
+  marty.walk(num_steps=steps, blocking=None)
 
 #Make marty kick with foot defined. Possible values of 'left' or 'right' 
 def martykick (foot):
   marty.kick(side=foot)
 
+#Marty dance routine in time specified. Side is defined as start foot.
+def martydance ():
+  marty.dance(side="right", move_time=5000)
+
+#Wave marty arms back and forth. acceptable values between -100 and 100 in time specified.
+def martyarms ():
+  marty.arms(-100, 100, 2000)
+  marty.arms(100, -100, 2000)
+  marty.arms(-100, 100, 2000)
+  marty.arms(100, -100, 2000)   
+  marty.arms(0, 0, 2000) 
+
+#Move Martys eyes to one of 4 poses [angry | excited | normal | wide] Can also accept a integer in angle number. -ive accepted.  
+def martyeyes (): 
+  marty.eyes("angry")
+  marty.eyes("excited")
+  marty.eyes("normal")
+  marty.eyes("wide")
+
+#Turn Marty on the spot. Pass left or right to the method and will be translated into 90 (Left) or -90 (Right) ~approx 10 - 12 steps needed to turn 90 degrees.
+def martyturn (direction): 
+  turndir=0
+  if direction == "left":
+    turndir = 90
+  elif direction == "right":
+    turndir = -90
+  marty.walk(num_steps=12, start_foot='auto',turn=turndir)
+
+#Play a sound from the internal marty speaker. Acceptable sound values of 20 - 20,000Hz. Maximum duration of 5000ms (5 Seconds)
+def martysound (): 
+  marty.play_sound(name_or_freq_start=200,freq_end=1000,duration=5000)
+
+#Make marty lean in a specified direction. Choice of [left | right | forward | backward]
+def martylean (direction): 
+  marty.lean(direction=direction)
+
 #Open Marty Communication Socket on Defined IP address
 #TODO Dont think this is correct, only holding open for one job - is this a network comms issue?
-marty=Marty('socket://'+ MartyIP, blocking=False)
+#marty=Marty('socket://'+ MartyIP, blocking=True)
 
 #martycalibrate() ##TODO This does not work - breaks calibration!!
-#martyalive()
-marty.get_battery_voltage()
+martyalive()
+martysound()
 martywalk(5)
-marty.kick(side='left', blocking=None)
-
-#martykick("left")
-#martykick("right")
-
-
-# marty.kick(side='left')
-#marty.arms(left_angle=100, right_angle=-100, move_time=2000,blocking=None)
-#time.sleep(5)
-#marty.kick(side='left', blocking=None)
-#marty.arms(left_angle=-100, right_angle=100, move_time=2000,blocking=None,)
-
-#marty.kick(side='left',)
-#marty.lean(direction='left',move_time=1000)
-
-#marty.play_sound(name_or_freq_start=200,freq_end=1000,duration=5000)
-
-#marty.eyes(pose_or_angle='angry',move_time=1000)
-
-
+martyturn("right")
+martykick("left")
+martykick("right")
+martydance()
+martylean("forward")
+martylean("back")
+martyarms()
+martyeyes()
